d7516eae34edf4b8d75e48bf40952bc22290871a
Victor Yarema <victor.yarema@gmail.com>
2016-09-17 11:13:49 +0300

    Type hotfix.

--- src/core/ngx_connection.c
+++ src/core/ngx_connection.c
@@ -365,7 +365,7 @@ ngx_set_inherited_sockets(ngx_cycle_t *cycle)
             continue;
         }
 
-        if (olen < sizeof(int) || timeout == 0) {
+        if (olen < (int)sizeof(int) || timeout == 0) {
             continue;
         }
 
